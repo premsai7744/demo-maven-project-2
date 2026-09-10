25c918df05da899d39fbf8023a2c4408e3eac9e5
premsai7744 <premsai7744@gmail.com>
2026-09-10 21:57:59 +0530

    commit.

--- maven-demo-project-2/src/main/java/com/premit/maven_demo_project_2/contactUs/Contact.java
+++ maven-demo-project-2/src/main/java/com/premit/maven_demo_project_2/contactUs/Contact.java
@@ -4,6 +4,7 @@ public class Contact {
 	public String contactUs() {
 		System.out.println("Contact.contactUs()");
 		System.out.println("ContactUs");
+		System.out.println("Contact@gmail.com");
 		return "1800-192-292";
 	}
 }
