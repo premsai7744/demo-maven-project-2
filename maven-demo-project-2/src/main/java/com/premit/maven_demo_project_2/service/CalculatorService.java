fcb147fb117b58036137c0c60631255662d79364
premsai7744 <premsai7744@gmail.com>
2026-09-10 21:14:31 +0530

    DEV : Initialized CalculatorService.java

---
+++ maven-demo-project-2/src/main/java/com/premit/maven_demo_project_2/service/CalculatorService.java
@@ -0,0 +1,10 @@
+package com.premit.maven_demo_project_2.service;
+
+public class CalculatorService {
+	
+	public int add(int i, int j) {
+		System.out.println("Add method involked.");
+		return i+j;
+	}
+
+}
