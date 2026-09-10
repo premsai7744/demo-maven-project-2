b615a46f5c3ba2c5bc3f27223c350280ad85b704
premsai7744 <premsai7744@gmail.com>
2026-09-10 19:44:07 +0530

    Initial commit.

---
+++ maven-demo-project-2/src/main/java/com/premit/maven_demo_project_2/App.java
@@ -0,0 +1,10 @@
+package com.premit.maven_demo_project_2;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
