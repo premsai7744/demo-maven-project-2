234f790b4b166fa7533b3847ee4030e602ff22f1
premsai7744 <premsai7744@gmail.com>
2026-09-10 21:57:16 +0530

    Merge branch 'master' of https://github.com/premsai7744/demo-maven-project-2.git

--- maven-demo-project-2/src/main/java/com/premit/maven_demo_project_2/contactUs/Contact.java
+++ maven-demo-project-2/src/main/java/com/premit/maven_demo_project_2/contactUs/Contact.java
@@ -3,8 +3,6 @@ package com.premit.maven_demo_project_2.contactUs;
 public class Contact {
 	public String contactUs() {
 		System.out.println("Contact.contactUs()");
-		System.out.println("ContactUs");
-		System.out.println("Contact.contactUs()");
 		return "1800-192-292";
 	}
 }
